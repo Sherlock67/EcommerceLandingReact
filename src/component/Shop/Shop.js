a31663876975382a05b109f59efe94503e3ca84d
Sherlock67 <alammushfiqul366@gmail.com>
2022-03-23 22:06:02 +0600

    final

--- src/component/Shop/Shop.js
+++ src/component/Shop/Shop.js
@@ -7,17 +7,26 @@ const Shop = () => {
     const [products,setProduct] = useState([]);
     const [cart,setCart] = useState([]);
     useEffect(()=>{
+        console.log('products load first before fetch');
         fetch('products.json')
         .then(res=>res.json())
         .then(data=>setProduct(data));
     },[]);
     useEffect(()=>{
+        console.log('products load first before fetch');
         const storedCart = getSortedCart();
+        const savedCart  = [];
         for(const id in storedCart){
             const addedProduct = products.find(product =>product.id === id)
-            console.log(addedProduct);
+            if(addedProduct){
+                  const quantity = storedCart[id];
+                  addedProduct.quantity = quantity;
+                  savedCart.push(addedProduct);
+            }
+           
         }
-    },[]);
+        setCart(savedCart);
+    },[products]);
     const handleAddToCart = (product) =>{
         console.log(product);
         const newCart = [...cart,product ];
